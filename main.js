209ec5ace9fe82c2aed71c2e6f9b2aece1b4f35b
Rimóczi Dániel <rimoczidani007@gmail.com>
2022-12-10 20:29:02 +0100

    I can move around a dot xd

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 
+const snakeboard = document.getElementById('snakeBoard')
+const ctx = snakeboard.getContext('2d') 
+const snakeBoardwidth = 200
+
+let dot = [{x:100, y:100}]
+ctx.fillStyle = "lightgreen"
+
+ctx.fillRect(dot[0].x, dot[0].y , 10, 10)
+
 
 document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener('keydown', function (event) {
+ 
         const {code} = event
         if (code === "KeyW") {
-            console.log("W pressed")
-        } else if (code === "ArrowUp") {
-            console.log("ArrowUp pressed")
-        }
+            if(dot[0].y == 0) {
+                return;
+            }
+            ctx.clearRect(dot[0].x, dot[0].y, 10, 10)
+      
+            dot[0].y -= 10
+            ctx.fillRect(dot[0].x, dot[0].y , 10, 10)
+        } else if (code === "KeyS") {
+            if(dot[0].y == 190) {
+                return;
+            }
+            ctx.clearRect(dot[0].x, dot[0].y, 10, 10)
+            
+            dot[0].y += 10
+            ctx.fillRect(dot[0].x, dot[0].y , 10, 10)
+        }      
+
 })
+
 });
